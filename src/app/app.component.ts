6f30acf281a00104c9c2c882450f3013eeb11b9a
rashminmhtc <rashminmhtc@gmail.com>
2018-06-06 09:28:23 +0530

    Vaidations commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {LOCAL_STORAGE, WebStorageService} from 'angular-webstorage-service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Phone book Forms';
+  constructor(@Inject(LOCAL_STORAGE) private storage: WebStorageService) {
+    this.saveInLocal(name, 'Rashmi');
+  }
+  saveInLocal(key, val): void {
+    console.log('recieved= key:' + key + 'value:' + val);
+    this.storage.set(key, val);
+    this.data[key]= this.storage.get(key);
+  }
+  getFromLocal(key): void {
+    console.log('recieved= key:' + key);
+    this.data[key]= this.storage.get(key);
+    console.log(this.data);
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { StorageServiceModule} from 'angular-webstorage-service';
 
 import { AppComponent } from './app.component';
 import { PhoneBookFormComponent } from './phone-book-form/phone-book-form.component';
+import { BirthDateValidatorDirective } from './birthDate.validator';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhoneBookFormComponent
+    PhoneBookFormComponent,
+    BirthDateValidatorDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    StorageServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
